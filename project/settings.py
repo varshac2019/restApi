64675a5ffae114475d39c0755bc8f981212b574b
varshac2019 <varsha.c@amazatic.com>
2019-06-11 15:18:25 +0530

    Configuring the app

--- project/settings.py
+++ project/settings.py
@@ -12,10 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import psycopg2
+import dj_database_url
 
-DATABASE_URL = os.environ['DATABASE_URL']
-
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +138,5 @@ REST_FRAMEWORK = {
      'SEARCH_PARAM':'q',
 
 }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
